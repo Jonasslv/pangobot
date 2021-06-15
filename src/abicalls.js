c14e13f32483d32faaf4b08b84b836a5439fdaa5
Jonasslv <jowsba@hotmail.com>
2021-06-15 08:35:11 -0300

    spore pools

--- src/abicalls.js
+++ src/abicalls.js
@@ -84,7 +84,13 @@ const PngStakingContracts = [
     },
     {
         stakingRewardAddress: '0x640D754113A3CBDd80BcCc1b5c0387148EEbf2fE'  //AVAX-SNOB
-    }
+    },
+    {
+        stakingRewardAddress: '0x12A33F6B0dd0D35279D402aB61587fE7eB23f7b0'
+    },
+    {
+        stakingRewardAddress: '0xd3e5538A049FcFcb8dF559B85B352302fEfB8d7C'
+    }  
 ]
 var provider = undefined;
 
